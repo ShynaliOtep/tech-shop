10dee0aa29ec47ec911cdcdb258d7bfdf8e16246
Shynali Otep <otep.sh@chocolife.kz>
2025-04-27 17:59:05 +0500

    Release Third and last

--- resources/views/navigation/bottom-navbar.blade.php
+++ resources/views/navigation/bottom-navbar.blade.php
@@ -8,17 +8,20 @@ $url =request()->path();
             <i class="large material-icons" @if($url == '/')  style="color: #e37509" @endif>
                 home
             </i>
+           <span>Главная</span>
         </a>
         <a href="/categories" class="bottom-navbar-item">
             <i class="large material-icons" @if($url == 'categories')  style="color: #e37509" @endif>
                 menu
             </i>
+            <span>Каталог</span>
         </a>
         @auth('clients')
         <a href="/profile/favorite" class="bottom-navbar-item">
             <i class="large material-icons" @if($url == 'profile/favorite')  style="color: #e37509" @endif>
                 favorite_border
             </i>
+            <span>Избранное</span>
         </a>
         @endauth
         <a href="/bonus" class="head-bonus-button">
