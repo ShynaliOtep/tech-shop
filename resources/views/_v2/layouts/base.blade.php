60aa518b3967e54bac2da0823a70a93758b1513a
Shynali Otep <otep.sh@chocolife.kz>
2025-07-03 09:40:09 +0500

    Redesign

--- resources/views/_v2/layouts/base.blade.php
+++ resources/views/_v2/layouts/base.blade.php
@@ -73,7 +73,50 @@
             </div>
         </section>
         @include('_v2.components.navigation.bottom-navbar')
+        @if(!session()->get('is_select_city'))
+            <div
+                style="z-index: 1003; display: block; opacity: 1; bottom: 0; transform: scaleX(1) scaleY(1);"
+                id="modal_location" class="modal" onclick="hideModalLocation()">
+                <div class="black-block simple-centred-block modal-block ">
+                    <span class="close" onclick="hideModalLocation()">
+                       <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M-6.99382e-07 16L6.56802 7.52941L9.50835 7.52941L16 16L13.0597 16L8.05728 9.26909L2.94033 16L-6.99382e-07 16Z" fill="#404040"/>
+                        <path d="M16 2.94707e-06L9.43198 8.47059L6.49165 8.47059L1.90735e-06 -6.99382e-07L2.94034 -3.59166e-07L7.94272 6.73091L13.0597 1.70928e-06L16 2.94707e-06Z" fill="#404040"/>
+                        </svg>
+                    </span>
+                    <p class="modal-title big-white-title mb-20">
+                        Выберите город
+                    </p>
+                    <p class="grey-s-light-text mb-20">
+                        Это нужно, чтобы показать товары и предложения, доступные именно в вашем регионе.
+                    </p>
+                    <div class="location-btns">
+                        <a   class="black-btn mini-btn mb-20"  href="{{route('selectCity', 1)}}">{{__('translations.Almaty')}}</a>
+                        <a  class="black-btn mini-btn mb-20" href="{{route('selectCity', 2)}}">{{__('translations.Astana')}}</a>
+                    </div>
+    {{--          --}}
+    {{--                <a href="/auth/register" class="black-btn">Создать аккаунт</a>--}}
+                </div>
+            </div>
+        @endif
     </div>
+    <style>
+        .location-btns {
+            display: flex;
+            justify-content: space-between;
+            gap: 20px;
+        }
+        @media (max-width: 600px) {
+            /*.location-btns {*/
+            /*    flex-wrap: wrap;*/
+            /*}*/
+        }
+    </style>
+    <script>
+        function hideModalLocation() {
+            document.getElementById('modal_location').style.display = 'none'
+        }
+    </script>
 <script src="{{asset('js/script.js?v=2')}}"></script>
 <script src="{{asset('js/dropdown.js?v=2')}}"></script>
 <script src="{{asset('js/materialize.js?v=2')}}"></script>
