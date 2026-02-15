60e8ddc326125997f2e2c9a0ab0f3c5bb0851c02
Shynali Otep <otep.sh@chocolife.kz>
2026-02-15 14:14:59 +0500

    updates

--- app/Orchid/Screens/OrderItem/OrderItemListScreen.php
+++ app/Orchid/Screens/OrderItem/OrderItemListScreen.php
@@ -2,7 +2,6 @@
 
 namespace App\Orchid\Screens\OrderItem;
 
-use App\Models\City;
 use App\Models\OrderItem;
 use App\Orchid\Filters\OrderItemNameFilter;
 use App\Orchid\Layouts\OrderItem\OrderItemFiltersLayout;
@@ -21,7 +20,7 @@ class OrderItemListScreen extends Screen
     public function query(): array
     {
         return [
-            'orderItems' => OrderItem::join('orders', 'order_items.order_id', '=', 'orders.id')->where('orders.city_id', City::getPlatformCity())->filters()->filters(OrderItemFiltersLayout::class)->paginate(),
+            'orderItems' => OrderItem::filters()->filters(OrderItemFiltersLayout::class)->paginate(),
         ];
     }
 
